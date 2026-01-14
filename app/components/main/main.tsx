80da052e286b17c73c0c6bcaca32ffff15b934da
Henrique Florencio <henrique.florencio@gmail.com>
2026-01-14 17:57:18 -0300

    style: renomeando Component Trabalhos

--- app/components/main/main.tsx
+++ app/components/main/main.tsx
@@ -2,17 +2,16 @@
 
 import { Intro } from "./intro/intro";
 import { Experiencia } from "./experience/experience";
-import { Trabalhos } from "./projects/projects";
+import { Projects } from "./projects/projects";
 import { Testimonials } from "./testimonials/testimonials";
 import { CallToAction } from "./action/action";
-import { useDriveContent } from "@/app/providers/drive-content/drive.provider";
 
 export function MainComponent() {
   return (
     <main>
       <Intro />
       <Experiencia />
-      <Trabalhos />
+      <Projects />
       <Testimonials />
       <CallToAction />
     </main>
