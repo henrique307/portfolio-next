796cade08b4c1bc443a96134fefaa54eb24c6583
Henrique Florencio <henrique.florencio@gmail.com>
2026-01-14 17:56:49 -0300

    feat: nova rota email

---
+++ app/api/mail/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { nome, email, mensagem } = body;
+
+    if (!nome || !email || !mensagem) {
+      return NextResponse.json({ error: "Dados inválidos" }, { status: 400 });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Contato do Site" <${process.env.EMAIL_USER}>`,
+      to: process.env.EMAIL_TO,
+      replyTo: email,
+      subject: `📩 Novo contato no Portfolio`,
+      html: `
+        <h2>Novo contato recebido</h2>
+        <p><strong>Nome:</strong> ${nome}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Mensagem:</strong></p>
+        <p>${mensagem}</p>
+      `,
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Erro ao enviar email" },
+      { status: 500 }
+    );
+  }
+}
